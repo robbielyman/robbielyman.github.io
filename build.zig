6c0c19a8fc351b39a8c659a62e06ca62a81aaa29
Robbie Lyman <rb.lymn@gmail.com>
2024-09-28 10:28:39 -0400

    feat: flesh out Math page; add Research and Teaching page content

--- build.zig
+++ build.zig
@@ -10,6 +10,22 @@ pub fn build(b: *std.Build) !void {
         .assets_dir_path = "assets",
         .static_assets = &.{
             "CNAME",
+            
+            "fonts/Taviraj-Regular.ttf",
+            "fonts/Taviraj-Black.ttf",
+            "fonts/Taviraj-BlackItalic.ttf",
+            "fonts/Taviraj-Bold.ttf",
+            "fonts/Taviraj-BoldItalic.ttf",
+            "fonts/Taviraj-Italic.ttf",
+            "fonts/Taviraj-Light.ttf",
+            "fonts/Taviraj-LightItalic.ttf",
+            
+            "fonts/Sarabun-Regular.ttf",
+            "fonts/Sarabun-Bold.ttf",
+            "fonts/Sarabun-BoldItalic.ttf",
+            "fonts/Sarabun-Italic.ttf",
+            "fonts/Sarabun-Light.ttf",
+            "fonts/Sarabun-LightItalic.ttf",
         },
         .build_assets = &.{
             .{
